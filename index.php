098ec751d0b2df5bb4bf960e324693a6803d151e
KimiMartua <88358228+KimiMartua@users.noreply.github.com>
2021-08-03 09:26:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Website kimi</title>
+	<link rel="stylesheet" type="text/css" href="css/kimi.css">
+</head>
+<body>
+	<div class="container">
+		<nav>
+			<ul>
+				<li>
+				<h1>KimiWebsite</h1>
+				</li>
+				<li>
+					<a href="#tentang">Tentang Saya</a>
+				</li>
+				<li>
+					<a href="#kontak">Contact</a>
+				</li>
+				<li>
+					<a href="#">Print</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="jumbotron">
+			<img src="img/PP.jpeg">
+		</div>
+		<h1 class="nama">KIMI MARTUA CASSANOVA SIMANJUNTAK</h1>
+		<div class="tentang">
+		<h1 id="tentang">TENTANG SAYA</h1>
+		<p>Saya seorang progammer,Saya suka memrogram suatu website,saya juga ingin belajar membuat sebuah aplikasi,saya juga seorang artist(penggambar).Saya selalu ingin belajar hal baru.</p>
+		</div>
+		<div class="projek">
+			<h1>PROJEK SAYA</h1>
+			<div class="foto1"><img src="img/MaidCostume.jpg"></div>
+			<div class="foto2"><img src="img/Random Chara-2.png"></div>
+			<div class="clean"></div>
+		</div>
+		<div class="contact">
+			<h1 id="kontak">KONTAK SAYA</h1>
+			<ul>
+				<li><a href="https://www.instagram.com/kimi.art13/">INSTRAGAM</a></li>
+				<li><a href="https://twitter.com/kimi_martua?s=09">FACEBOOK</a></li>
+				<li><a href="https://www.facebook.com/kimi.martua.79">TWITTER</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html>
